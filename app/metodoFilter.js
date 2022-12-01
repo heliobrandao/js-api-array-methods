16eec87607f58432dc3b8a4d326fb2ab59599e4a
heliobrandao <heliob79@gmail.com>
2022-12-01 19:16:28 -0300

    lesson-05

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -4,6 +4,27 @@ botoes.forEach(btn => btn.addEventListener('click', filtrarLivros));
 function filtrarLivros() {
   const elementobtn = document.getElementById(this.id);
   const categoria = elementobtn.value
-  let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria);
+  let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() : filtarPorCategoria(categoria);
   exibirOsLivrosNaTela(livrosFiltrados);
+  if (categoria == 'disponivel') {
+    const valorTotal = calcularValorTotalDeLivrosDisponiveis(livrosFiltrados);
+    console.log(valorTotal)
+    exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal);
+  }
+}
+
+function filtarPorCategoria(categoria) {
+  return livros.filter(livro => livro.categoria == categoria);
+}
+
+function filtrarPorDisponibilidade() {
+  return livros.filter(livro => livro.quantidade > 0);
+}
+
+function exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal) {
+  elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
+  <div class="livros__disponiveis">
+    <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
+  </div>
+  `
 }
